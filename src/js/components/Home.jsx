514ca20516e66c913670d9cf69bfb0c4a9fe0d30
Alexis Rodriguez <alexisrrh123@gmail.com>
2026-03-13 13:17:51 +0000

    proyecto funcional

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React,{useEstate, useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+const Home = () => {
+
+const [text,setText]=useState("");
+const [lista,setLista]=useState([]);
+console.log(text,lista);
+
+  const handleKeyDown = (event) => {
+   
+let nuevoValor= event.target.value;
+    
+    if (event.key === 'Enter') {
+		   setLista([...lista, nuevoValor]);
+		   setText("");
+  
+    }
+  };
+  
+
+  const eliminar=(index)=>{
+	 setLista(lista.filter((item,i)=> i !== index));
+
+  }
+
+	return (
+		<div id="divPadre" className="text-center">
+            <h1>todos</h1>
+
+  <div id="divHijo" className="mb-3">
+   
+    <input id="inputPrincipal" type="text" className="form-control" aria-describedby="emailHelp" onKeyDown={handleKeyDown} onChange={(e)=>{setText(e.target.value)}} value={text}/>
+
+	<ul id="lista">{lista.map((item,index) =>(
+	<li id="nuevoLi" key={index}>{item}<button onClick={() => eliminar(index)} >xs</button></li>
+	))}</ul>
+</div>
+ 
+
+		
 		</div>
 	);
 };
